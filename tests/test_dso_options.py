4cadb1123cce5c844b512ac179c3594f5522e6d2
Fiodar Stryzhniou <fedor_qd@mail.ru>
2018-11-30 20:21:06 +0300

    test_dso_options.py: more pretty print tests results

--- tests/test_dso_options.py
+++ tests/test_dso_options.py
@@ -11,26 +11,26 @@ tgttype=r" --targettype=implib"
 
 
 dsodefTests=(
-(elf2e32+defin+dsoout+linkas+tgttype, "Simple def2dso creation. Options are: %s\n"),
-(elf2e32+""" --elfinput="libcrypto.SDK.dso" """ + """ --defoutput="tmp\dso2def.(01).def" """,
-"dso2def creation with simplified syntax. Options are: %s\n"),
+(elf2e32+defin+dsoout+linkas+tgttype, "Simple def2dso creation.\n Options are: %s\n"),
+(elf2e32+""" --elfinput="tmp\libcrypto{000a0000}.def2dso.dso" """ + """ --defoutput="tmp\dso2def.(01).def" """,
+"dso2def creation with simplified syntax.\n Options are: %s\n"),
 (elf2e32+""" --definput="tmp\dso2def.(01).def" """ + """ --dso="tmp\dso2def2dso.(02).dso" """,
-"Make dso from def generated from dso. Options are: %s\n"),
+"Make dso from def generated from dso.\n Options are: %s\n"),
 (elf2e32+defin+ """--dso="tmp\def2dso.(03).dso""", "Make dso from def which made from dso... Options are: %s\n"),
 (elf2e32+"""--elfinput="tmp\def2dso.(03).dso""" + """ --defoutput="tmp\def2dso2def.(04).def" """,
-"Make def from dso which made from def... Options are: %s\n"),
+"Make def from dso which made from def...\n Options are: %s\n"),
 )
 
 def SuceededTests(*args):
    """These tests must alwais pass!"""
    tmp=args[0]
+   str=tmp[1] %tmp[0]
    try:
       print "\n"+tmp[0]
       subprocess.check_call(tmp[0])
    except:
-      print "Unexpectable test failure: %s\n" %tmp[0]
+      print "Unexpectable test failure:\n %s\n" %str
    else:
-      str=tmp[1] %tmp[0]
       print "Test succeeded: %s!\n" %str
 
 def run():
